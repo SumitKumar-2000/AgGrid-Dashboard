e8847c57c080e9ae7e954ee9055244401e0be688
SumitKumar-2000 <sumitkumarracer@gmail.com>
2022-11-17 15:24:18 +0530

    [TimeSeriesChart.js] dynamic calculations done to reflect changes on timeSeries and pi chart

--- src/components/TimeSeriesChart.js
+++ src/components/TimeSeriesChart.js
@@ -1,51 +1,94 @@
-import React, { useState } from 'react'
+import React  from 'react'
 import { TimeSeriesChartContainer } from './StyledComponents/TimeSeriesChart.styled'
+import useFilteredBookingStore from '../utils/HotelBookingsStore'
+import HotelBookings from '../data/hotel_bookings'
 
 // charts
 import Chart from 'react-apexcharts'
 
 const TimeSeriesChart = () => {
 
-  const [lineSeriesChartData,setlineSeriesChartData] = useState([{
-    name : "Visitors per day",
-    data : [63, 72, 77, 72, 63, 73, 38, 42, 45, 75, 58, 102, 83, 45, 55, 54, 106, 71, 63, 78, 29, 17, 34, 70, 91, 35, 82, 32, 43, 43, 58, 70, 26, 26, 28, 31, 30, 53, 64, 32]
-  }])
-  const [lineChartOptions,setLineChartOptions] = useState({
-    chart : {
-      id : "visitors per day"
-    },
-    xaxis : {
-      categories : ["July 1","July 2","July 3","July 4","July 5","July 6","July 7","July 8","July 9","July 10","July 11","July 12","July 13","July 14","July 15","July 16","July 17","July 18","July 19","July 20","July 21","July 22","July 23","July 24","July 25","July 26","July 27","July 28","July 29","July 30","July 31","August 1","August 2","August 3","August 4","August 5","August 6","August 7","August 8","August 9"],
-    },
-    yaxis : {
-      title : {
-        text : "Visitors_Count"
-      }
-    },
-    colors : ["#34A0A4"],
-    title :{
-      text : "VISITORS PER DAY",
-      style : {
-        fontSize : "20"
-      }
-    }
-  })
-  const [piSeriesChartData,setPiSeriesChartData] = useState([
-    1990,213,24
-  ])
+  // zustand config, fetching filtered bookings on the basis of start and end date
+  const {startDate,endDate} = useFilteredBookingStore((state)=> state.BookingDates)
+
+  const visitorData = {
+    dates : [],
+    totalVisitors : [],
+    adults : [],
+    children : [],
+    babies : [],
+  }
+
+  const set = new Set();
+  HotelBookings.filter(item => `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` >= startDate  && `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` <= endDate).map(item => set.add(`${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}`))
+
+  for(const element of set){
+    visitorData.dates.push(element)
+  }
+
+  // total visitors calculation
+  for(const element of visitorData.dates){
+    const visitorsPerDate = HotelBookings.filter(item => `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` === element).reduce((total,curr)=>{
+      return total + curr.adults + curr.children + curr.babies
+    },0)
+    visitorData.totalVisitors.push(visitorsPerDate)
+  }
+
+  // adultvisitors calculation
+  const adultCount = HotelBookings.filter(item => `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` >= startDate  && `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` <= endDate).reduce((total,curr)=>{
+    return total + curr.adults
+  },0) 
+  visitorData.adults.push(adultCount)
+  
+  // childrenvisitors calculation
+  const childrenCount = HotelBookings.filter(item => `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` >= startDate  && `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` <= endDate).reduce((total,curr)=>{
+    return total + curr.children
+  },0) 
+  visitorData.children.push(childrenCount)
+  
+  // babyvisitors calculation
+  const babiesCount = HotelBookings.filter(item => `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` >= startDate  && `${item.arrival_date_year}-${item.arrival_date_month}-${item.arrival_date_day_of_month}` <= endDate).reduce((total,curr)=>{
+    return total + curr.babies
+  },0) 
+  visitorData.babies.push(babiesCount)
+
 
   return (
     <TimeSeriesChartContainer>
         <div className='linePlot'>
           <Chart
             type='line'
-            series={lineSeriesChartData}
-            options={lineChartOptions}
+            series={[{
+              name : "Visitors per day",
+              data : [...visitorData.totalVisitors]
+            }]}
+            options={{
+              chart : {
+                id : "visitors per day"
+              },
+              xaxis : {
+                categories : [...visitorData.dates],
+              },
+              yaxis : {
+                title : {
+                  text : "Visitors_Count"
+                }
+              },
+              colors : ["#34A0A4"],
+              title :{
+                text : "VISITORS PER DAY",
+                style : {
+                  fontSize : "20"
+                }
+              }
+            }}
             height={300}
           />
         </div>
         <div className='doughnutPlot'>
-          <Chart type='donut' series={piSeriesChartData} options={{
+          <Chart type='donut' 
+              series={[...visitorData.adults, ...visitorData.children, ...visitorData.babies]} 
+              options={{
               chart: {
                 type: 'donut',
               },
